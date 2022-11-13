53bd49f5cb90c51e1fd319526a75a1a2c044e188
Frank Pham <ff.pbphuoc@gmail.com>
2022-11-14 03:31:28 +1000

    clean code

--- CMK3D1/src/day12/NumberAppearOddTimes.java
+++ CMK3D1/src/day12/NumberAppearOddTimes.java
@@ -8,26 +8,26 @@ public class NumberAppearOddTimes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = {1,2,3,4,5,6,1,2,3,4,5,100,400, 15,15,15,15,15,15,15,15};
+		int[] arr = { 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 100, 400, 15, 15, 15, 15, 15, 15, 15, 15 };
 		findOddTimesAppearNum(arr);
 
 	}
 
 	public static void findOddTimesAppearNum(int[] arr) {
 		Map<Integer, Integer> occurences = new HashMap<Integer, Integer>();
-		//loop through arr length = N -> O (N logN) 
-		for(int value: arr) {
-			//O(1)
-			if(occurences.containsKey(value)) {
+		// loop through arr length = N -> O (N logN)
+		for (int value : arr) {
+			// O(1)
+			if (occurences.containsKey(value)) {
 				// O(logN)
 				occurences.put(value, occurences.get(value) + 1);
-			}else {
+			} else {
 				occurences.put(value, 1);
 			}
 		}
-		//ON
-		for(Entry<Integer, Integer> entry: occurences.entrySet()) {
-			if(entry.getValue() % 2 != 0)
+		// ON
+		for (Entry<Integer, Integer> entry : occurences.entrySet()) {
+			if (entry.getValue() % 2 != 0)
 				System.out.println(entry.getKey() + " " + entry.getValue() + " times");
 		}
 		// -> Total Time Complexity: O (N logN) + O(N)
--- CMK3D1/src/day12/SquareInteger.java
+++ CMK3D1/src/day12/SquareInteger.java
@@ -2,47 +2,43 @@ package day12;
 
 public class SquareInteger {
 
-	static int count;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Square Integer from 17 to 24: " + squareInteger(17,24));
-		System.out.println("Square Integer from 3 to 9: " + squareInteger(3,9));
-		System.out.println("Square Integer from 2 to 8: " + squareInteger(2,8));
-		System.out.println("Square Integer from 0 to 100: " + squareInteger(0,100));
-		System.out.println("Square Integer from 81 to 100: " + squareInteger(81,100));
-//		System.out.println("count: " + count);
+		System.out.println("Square Integer from 17 to 24: " + squareInteger(17, 24));
+		System.out.println("Square Integer from 3 to 9: " + squareInteger(3, 9));
+		System.out.println("Square Integer from 2 to 8: " + squareInteger(2, 8));
+		System.out.println("Square Integer from 0 to 100: " + squareInteger(0, 100));
+		System.out.println("Square Integer from 81 to 100: " + squareInteger(81, 100));
 	}
-	
+
 	public static int sqrtIntNum(int num) {
 		int l = 0;
 		int r = num;
 		int m = 0;
-		//O logN
-		while(l <= r) {
-//			count++;
+		// O logN
+		while (l <= r) {
 			m = (l + r) / 2;
-			if(m*m < num)
+			if (m * m < num)
 				l = m + 1;
-			else if(m*m > num)
+			else if (m * m > num)
 				r = m - 1;
 			else
 				return m;
-		}	
-		m = (m*m < num) ? m + 1 : m;	
+		}
+		m = (m * m < num) ? m + 1 : m;
 		return m;
 	}
-	
+
 	public static int squareInteger(int start, int end) {
 		int sqrt = sqrtIntNum(start);
 		int countSquareInteger = 0;
-		//worst case: sqrt run from 0 to sqrt(end) -> O sqrt(end)
-		while(sqrt*sqrt <= end) {
-//			++count;
+		// worst case: sqrt run from 0 to sqrt(end) -> O sqrt(end)
+		while (sqrt * sqrt <= end) {
 			++countSquareInteger;
 			++sqrt;
-		}	
+		}
 		return countSquareInteger;
-		//->total:  O sqrt(end)
+		// ->total: O sqrt(end)
 	}
 
 }
